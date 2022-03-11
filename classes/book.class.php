fc97867553c7b8eca2dc799692fad51c28291d93
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2022-03-11 08:37:49 +0200

    Create insert, delete and showSpecialAttr functions

--- classes/book.class.php
+++ classes/book.class.php
@@ -1,1 +1,40 @@
 <?php
+
+class Book extends Product
+{
+    protected $weight;
+
+    public static function getProducts()
+    {
+        parent::start();
+        $objs = parent::$db->ObjectBuilder()->rawQuery('SELECT * from products where weight IS NOT NULL');
+        foreach($objs as $idx=>$dbObj) {
+            $book = new Book();
+            $book->name = $dbObj->name;
+            $book->price = $dbObj->price;
+            $book->sku = $dbObj->SKU;
+            $book->weight = $dbObj->weight;
+            $objs[$idx] = $book;
+        }
+        return $objs;     // array of Book objects
+    }
+
+    public function insertProduct($data)
+    {
+        $name = $data['name'];
+        $sku = $data['SKU'];
+        $price = $data['price'];
+        $weight = $data['weight'];
+        $dataInsert = Array ( "name" => $name ,
+            "SKU" => $sku ,
+            "price" => $price,
+            "weight" => $weight
+        );
+        return parent::$db->insert(parent::$table, $dataInsert);
+    }
+
+    public function showSpecialAttr()
+    {
+        return "Weight: ".$this -> weight." KG";
+    }
+}
