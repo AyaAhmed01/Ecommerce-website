6b39d87a3d57037cdd3f21e904bd6c37821b4f00
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2022-03-11 08:40:26 +0200

    Create insert, delete and showSpecialAttr functions

--- classes/dvd.class.php
+++ classes/dvd.class.php
@@ -1,1 +1,40 @@
 <?php
+
+class Dvd extends Product
+{
+    protected $size;
+
+    public static function getProducts()
+    {
+        parent::start();
+        $objs = parent::$db->ObjectBuilder()->rawQuery('SELECT * from products where size IS NOT NULL');
+        foreach($objs as $idx=>$dbObj) {
+            $dvd = new Dvd();
+            $dvd->name = $dbObj->name;
+            $dvd->price = $dbObj->price;
+            $dvd->sku = $dbObj->SKU;
+            $dvd->size = $dbObj->size;
+            $objs[$idx] = $dvd;
+        }
+        return $objs;     // array of Dvd objects
+    }
+
+    public function insertProduct($data)
+    {
+        $name = $data['name'];
+        $sku = $data['SKU'];
+        $price = $data['price'];
+        $size = $data['size'];
+        $dataInsert = Array ( "name" => $name ,
+            "SKU" => $sku ,
+            "price" => $price,
+            "size" => $size
+        );
+        return parent::$db->insert(parent::$table, $dataInsert);
+    }
+
+    public function showSpecialAttr()
+    {
+        return "Size: $this->size MB";
+    }
+}
--- classes/furniture.class.php
+++ classes/furniture.class.php
@@ -1,1 +1,39 @@
 <?php
+class Furniture extends Product
+{
+    protected $length, $width, $height;
+
+    public static function getProducts()
+    {
+        parent::start();
+        $objs = parent::$db->ObjectBuilder()->rawQuery('SELECT * from products where width IS NOT NULL');
+        foreach($objs as $idx=>$dbObj) {
+            $furniture = new Furniture();
+            $furniture->name = $dbObj->name;
+            $furniture->price = $dbObj->price;
+            $furniture->sku = $dbObj->SKU;
+            $furniture->length = $dbObj->length;
+            $furniture->height = $dbObj->height;
+            $furniture->width = $dbObj->width;
+            $objs[$idx] = $furniture;
+        }
+        return $objs;     // array of Furniture objects
+    }
+
+    public function insertProduct($data)
+    {
+        $dataInsert = Array ( "name" => $data['name'] ,
+            "SKU" => $data['SKU'] ,
+            "price" => $data['price'],
+            "length" => $data['length'],
+            "width" => $data['width'],
+            "height" => $data['height']
+        );
+        return parent::$db->insert(parent::$table, $dataInsert);
+    }
+
+    public function showSpecialAttr()
+    {
+        return "Dimension: $this->length X $this->width X $this->height";
+    }
+}
