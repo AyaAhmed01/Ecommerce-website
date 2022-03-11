97460c67858b798c8743236fd0be260d4b6d0704
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2022-03-11 08:36:28 +0200

    Build header bar and dynamic form with required field validation

--- add-product.php
+++ add-product.php
@@ -1,31 +1,68 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php
+include('includes/header.inc.php');
+include 'includes/autoload.inc.php';
+?>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<nav class="navbar navbar-light navbar-expand-lg" style="background-color: #cf0b0c;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="assets/logo.png" width=40 alt="">
+            <span style="color: white; font-size: medium">Product Add</span>
+        </a>
+    </div>
 
-    <title>Amazing website</title>
-</head>
-<body>
-<select id="productType">
-    <option id="Book" selected>Book</option>
-    <option id="DVD">DVD</option>
-    <option id="Furniture">Furniture</option>
-</select>
+    <form class="container-fluid justify-content-start">
+        <button class="btn btn-sm btn-outline-secondary" type="button">
+            <a class="nav-link" href="index.php">Cancel</a>
+        </button>
+        <button class="btn btn-sm btn-outline-secondary" name="submit" form="product_form" value="Save">Save</button>
+    </form>
+</nav>
 
-<div id="specialData"></div>
+
+<div class="container">
+    <form class="was-validated" id="product_form" method="POST" action="classes/insert-product.class.php">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" required name="name" class="form-control" id="name" pattern="^[a-z]+[a-z ]*i">  // start with letter could end with letter or space
+            <div class="invalid-feedback">Please, submit required data</div>
+        </div>
+        <div class="form-group">
+            <label for="price">Price</label>
+            <input type="number" required class="form-control" name="price" id="price" pattern="\d+">
+            <div class="invalid-feedback">Please, submit required data</div>
+        </div>
+
+        <div class="form-group">
+            <label for="sku">SKU</label>
+            <input type="text" required class="form-control" name="SKU" id="sku" pattern="[\w]">
+            <div class="invalid-feedback">Please, submit required data</div>
+        </div>
+
+        <label for="productType">Product type</label>
+        <select class="form-select" aria-label="Default select example" id="productType" name="product-type" required>
+            <option selected value="Book">Book</option>
+            <option value="DVD">DVD</option>
+            <option value="Furniture">Furniture</option>
+        </select>
+        <div id="specialData" class="form-group">
+            <label for="weight">Weight (KG) </label>
+            <input type="number" required class="form-control" name="weight" id="weight" pattern="\d+">
+            <small>*Please provide book weight in KG*</small>
+        </div>
+    </form>
+</div>
+
+<?php include('includes/footer.inc.php'); ?>
+
 </body>
 
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script type="text/javascript">
     jQuery(document).ready(function () {
         jQuery("#productType").change(function () {
-            jQuery("#specialData").load("includes/formData.inc.html" + " #Book");
+            let selected_val = $("#productType option:selected").val();
+            jQuery("#specialData").load("includes/formData.inc.html #" + selected_val);
         });
     });
 </script>
--- index.php
+++ index.php
@@ -1,1 +1,72 @@
 <?php
+include 'includes/autoload.inc.php';
+include('includes/header.inc.php');
+?>
+
+<!--Here I show the list of products-->
+
+<nav class="navbar navbar-light navbar-expand-lg" style="background-color: #cf0b0c;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="assets/logo.png" width=40 alt="">
+            <span style="color: white; font-size: medium">Product List</span>
+        </a>
+    </div>
+
+    <form class="container-fluid justify-content-start">
+        <button class="btn btn-sm btn-outline-secondary" type="button">
+            <a class="nav-link" href="add-product.php">ADD</a>
+        </button>
+        <button class="btn btn-sm btn-outline-secondary" type="submit" form="delete-form">MASS DELETE</button>
+    </form>
+</nav>
+
+<div class="container">
+    <form action="classes/delete-products.class.php" method="POST" id="delete-form">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <!-- Now each card is (col -> card -> in card body put info and the form checkbox -->
+            <!-- foreach product -->
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input delete-checkbox" name="deletedIds[]"
+                                   value="3r4df">
+                        </div>
+                        <p class="card-text" style="align-content: center; padding: 20px;">This is a longer card with
+                            supporting text below as a natural lead-in to additional content. This content is a little
+                            bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input delete-checkbox" name="deletedIds[]"
+                                   value="2">
+                        </div>
+                        <p class="card-text" style="align-content: center; padding: 20px;">This is a longer card with
+                            supporting text below as a natural lead-in to additional content. This content is a little
+                            bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input delete-checkbox" name="deletedIds[]"
+                                   value="3">
+                        </div>
+                        <p class="card-text" style="align-content: center; padding: 20px;">This is a longer card with
+                            supporting text below as a natural lead-in to additional content. This content is a little
+                            bit longer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+<?php include('includes/footer.inc.php'); ?>
