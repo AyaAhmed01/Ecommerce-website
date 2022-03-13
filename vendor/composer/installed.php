4aca3838bdd17ebc6e0e11d906a82382013dfcfe
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2022-03-13 09:34:41 +0200

    Add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8e17d3df2bc4b61b9208c211e39f5e4e5e0676b9',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8e17d3df2bc4b61b9208c211e39f5e4e5e0676b9',
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '2.2.0',
+            'version' => '2.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'reference' => 'b301c98f19414d836fdaa678648745fcca5aeb4f',
+            'dev_requirement' => true,
+        ),
+    ),
+);
