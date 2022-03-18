e25fa00cd46dc9ab7085f9f8f817dfd7d82c7040
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2022-03-18 10:32:33 +0200

    Add css styles link

--- src/includes/header.php
+++ src/includes/header.php
@@ -5,23 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
+    <!-- CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="/../src/Assets/css/styles.css" type="text/css">
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" type=”text/javascript”></script>
-<!--    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>-->
-
-
-    <!-- Latest compiled and minified CSS -->
-<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">-->
-
-    <!-- jQuery library -->
-<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
-
-    <!-- Latest compiled JavaScript -->
-<!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>-->
-
-    <!--    <link href="/../vendor/twbs/bootstrap/dist/css/bootstrap-grid.min.css" rel="stylesheet" type="text/css"/>-->
     <title>Amazing website</title>
 </head>
-<body>
+
 
